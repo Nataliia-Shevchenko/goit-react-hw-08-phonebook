d926d1daba3799c00a5b20f0befc3aa0a23aa782
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-06-26 21:09:53 +0300

    update layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,18 @@
 import { Suspense, useEffect } from 'react';
 import { AppBarComp } from './AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
-import { useDispatch} from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import { refreshUser } from 'redux/auth/auth-operations';
 
 export const Layout = () => {
 
-  // const {token, user} = useSelector(state => state.auth)
+  const {token, user} = useSelector(state => state.auth)
   const dispatch = useDispatch();
   
   useEffect(() => {
-    // token && !user && 
+    token && !user && 
     dispatch(refreshUser());
-  }, [dispatch]);
+  }, [dispatch, token, user]);
 
   return (
     <div>
