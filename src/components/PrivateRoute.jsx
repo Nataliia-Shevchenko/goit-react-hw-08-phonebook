91d1c987d72217454908c9c1fe9cf91a182c41a7
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-06-26 21:13:31 +0300

    change routes

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
-// import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
+import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
 
 const PrivateRoute = ({ children }) => {
-  const isAuth = useSelector(state => state.auth.token);
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
+  // const isAuth = useSelector(state => state.auth.token);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   const location = useLocation();
 
-  return isAuth ? children : <Navigate to="/login" state={location}/>;
+  return isLoggedIn ? children : <Navigate to="/login" state={location}/>;
 };
 
 export default PrivateRoute;
--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -1,12 +1,12 @@
 import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
-// import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
+import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
 
 const PublicRoute = ({ children }) => {
-  // const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isAuth = useSelector(state => state.auth.token);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  // const isAuth = useSelector(state => state.auth.token);
   const location = useLocation();
-  return !isAuth ? children : <Navigate to={location.state ?? '/'} />;
+  return !isLoggedIn ? children : <Navigate to={location.state ?? '/'} />;
 };
 
 export default PublicRoute;
